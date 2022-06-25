4d2e953e0e5f7bdde2a4e8573875c36d9fb99027
mahjabin_mimi <105810160+mahjabinmimi@users.noreply.github.com>
2022-06-25 10:43:44 +0600

    Add files via upload

---
+++ lab ex.22.c
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+	int n;
+	printf("Enter n term: ");
+	scanf("%d", &n);
+	printf("Sum of the series 1+11+111+..n terms: ");
+	for (int i = 1; i <= n; i++) printf("%d", i);
+	return 0;
+}
